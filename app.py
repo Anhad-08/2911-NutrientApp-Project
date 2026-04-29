d42312c9008b4a4e299466d4df04b158e1d00b5e
Anhad <anhad2001.chandi@gmail.com>
2026-04-29 11:00:45 -0700

    created an app route

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"message" : "this is test for flask implementation"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
