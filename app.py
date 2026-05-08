aeb4391b75c17fc24948daa84566709364a9237f
Anhad <anhad2001.chandi@gmail.com>
2026-05-07 21:04:51 -0700

    functions and endpoints created for app.py

--- app.py
+++ app.py
@@ -1,50 +1,6 @@
 from flask import *
 
-from sqlalchemy import select
-from pathlib import Path
-from db import db
-from models import *
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-
-
-def updateFoolMealCSV():
-    data = "meal_id,food_id,amount"
-    for each in FoodMeal.query.all():
-        data += f"\n{each.meal_id},{each.food_id},{each.amount}"
-    with open("data/foodmeal.csv", "w") as f:
-        f.write(data)
-
-def updateMealCSV():
-    data = "user_id,name"
-    for each in Meal.query.all():
-        data += f"\n{each.user_id},{each.name}"
-    with open("data/meals.csv", "w") as f:
-        f.write(data)
-    updateFoolMealCSV()
-
-def updateUsersCSV():
-    data = "name,phone,height,weight,gender,age,tdee,password"
-    for each in User.query.all():
-        data += f"\n{each.name},{each.phone},{each.height},{each.weight},{each.gender},{each.age},{each.tdee},{each.password}"
-    with open("data/users.csv", "w") as f:
-        f.write(data)
-
-def updateFoodCSV():
-    data = "name,calories,protein,fat,carbs"
-    for each in Food.query.all():
-        data += f"\n{each.name},{each.calories},{each.protein},{each.fat},{each.carbs}"
-    with open("data/food.csv", "w") as f:
-        f.write(data)
-
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "secretAgileCourse"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-app.instance_path = Path(".").resolve()
-
-db.init_app(app)
-login_manager = LoginManager()
-login_manager.login_view = "login"
-login_manager.init_app(app)
 
 @app.route("/")
 def home():
@@ -56,10 +12,33 @@ def add_new():
 
 @app.route("/users")
 def users_list():
-    statement = select(User).order_by(User.id)
+    return "Users list page coming soon"
+
+
+@app.route("/food")
+def food_list():
+    return "Food list page coming soon"
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/loginpost", methods=["POST"])
+def login_post():
+    username = request.form["username"]
+    password = request.form["password"]
+    statement = select(User).where(User.name == username, User.password == password)
     records = db.session.execute(statement)
-    data = records.scalars().all()
-    return render_template("users.html", users = data)
+    user = records.scalars().first()
+
+    if not user:
+        flash("Invalid username or password")
+        return redirect(url_for("login"))
+    login_user(user)
+    return redirect(url_for("user_info", user_id=user.id))
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)@app.route("/users")
+
+
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from 2911-nutrientapp-project!")
-
-
-if __name__ == "__main__":
-    main()
