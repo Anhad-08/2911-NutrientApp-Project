c3b36a8acb50159eff423ae708aac1e099f3d344
Anhad <anhad2001.chandi@gmail.com>
2026-05-15 00:41:37 -0700

    project done

--- manage.py
+++ manage.py
@@ -1,20 +1,49 @@
-from db import db
-from models import User
-from app import app
 import csv
+import random
+from sqlalchemy.sql import functions as func
+from db import db
+from models import User, Meal, Food, FoodMeal
+from app import app
 
 
 def GetUser():
     with open("./data/users.csv", "r") as f:
         data = csv.DictReader(f)
         for each in data:
-            obj = User(
-                name=each["name"],
-                password=each["password"]
-            )
-            db.session.add(obj)
+            obj = User(name=each["name"], phone=each["phone"], 
+                                height=each["height"], weight=each["weight"], 
+                                gender=each["gender"], age=each["age"],
+                                tdee=each["tdee"], password=each["password"])
+        db.session.add(obj)
     db.session.commit()
 
+def GetFood():
+    with open("./data/food.csv", "r") as f:
+        data = csv.DictReader(f)
+        for each in data:
+            obj = Food(name=each["name"], calories=each["calories"], 
+                       protein=each["protein"], fat=each["fat"], 
+                       carbs=each["carbs"])
+            db.session.add(obj)
+        db.session.commit()
+
+
+def GetMeal():
+    with open("./data/meals.csv", "r") as f:
+        data = csv.DictReader(f)
+        for each in data:
+            obj = Meal(name=each["name"], user_id=each["user_id"])
+            db.session.add(obj)
+        db.session.commit()
+
+def GetFoodMeal():
+    with open("./data/foodmeal.csv", "r") as f:
+        data = csv.DictReader(f)
+        for each in data:
+            obj = FoodMeal(meal_id=each["meal_id"], food_id=each["food_id"], 
+                           amount=each["amount"])
+            db.session.add(obj)
+        db.session.commit()
 
 def LoadData():
     
@@ -22,10 +51,9 @@ def LoadData():
         db.drop_all()
         db.create_all()
         GetUser()
-        # OPTIONAL: create a test user
-        test = User(name="anhad", password="123")
-        db.session.add(test)
-        db.session.commit()
+        GetFood()
+        GetMeal()
+        GetFoodMeal()
 
 if __name__ == "__main__":
     LoadData()
