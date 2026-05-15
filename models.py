f6c6121180c0f2abf71b067d905b0b95b4e2ae59
NTDuongx315 <tienduong3105@gmail.com>
2026-05-14 20:32:13 -0700

    Update models.py

--- models.py
+++ models.py
@@ -29,6 +29,43 @@ class User(db.Model, UserMixin):
             calories += macros["calories"]
         return {"fat": fat, "protein": protein, "carbs": carbs, "calories": calories}
 
+class Meal(db.Model):
+    id = mapped_column(Integer, primary_key=True) 
+    user_id = mapped_column(Integer, ForeignKey(User.id), nullable=False)
+    name = mapped_column(String(200), nullable=False) 
+    user = relationship("User", back_populates="meals")
+    foodlist = relationship("FoodMeal", back_populates="meal")
 
+    def getMacros(self):
+        fat = 0
+        protein = 0
+        carbs = 0
+        calories = 0
+        for each in self.foodlist:
+            food = each.food
+            fat += food.fat * each.amount/100
+            protein += food.protein * each.amount/100
+            carbs += food.carbs * each.amount/100
+            calories += food.calories * each.amount/100
+        return {"fat": fat, "protein": protein, "carbs": carbs, "calories": calories}
+
+class Food(db.Model):
+    id = mapped_column(Integer, primary_key=True) 
+    name = mapped_column(String(200), nullable=False, unique=True) 
+    calories = mapped_column(Float, nullable=False) 
+    protein = mapped_column(Float, nullable=False)
+    carbs = mapped_column(Float, nullable=False)
+    fat = mapped_column(Float, nullable=False)
+
+    def toDict(self):
+        return {"id": self.id, "name": self.name, "calories": self.calories, "protein": self.protein, "fat": self.fat, "carbs": self.carbs}
+
+class FoodMeal(db.Model):
+    id = mapped_column(Integer, primary_key=True) 
+    meal_id = mapped_column(Integer, ForeignKey(Meal.id), nullable=False)
+    food_id = mapped_column(Integer, ForeignKey(Food.id), nullable=False)
+    amount = mapped_column(Float, nullable=False)
+    meal = relationship("Meal")
+    food = relationship("Food")
 
 # if user input about 2 eggs, that quantity will automatically be converted to 2*50g
